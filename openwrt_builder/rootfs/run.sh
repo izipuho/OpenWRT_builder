ca476028e4bc960b8d5e04fd7dc7e577e799b6c3
izipuho <58221294+izipuho@users.noreply.github.com>
2026-01-27 09:20:40 +0300

    playing around with run.sh

--- openwrt_builder/rootfs/run.sh
+++ openwrt_builder/rootfs/run.sh
@@ -1,8 +1,2 @@
 #!/command/with-contenv sh
-set -euo pipefail
-
-python3 /usr/local/bin/health_server.py &
-
-# TODO: тут запускай основной процесс аддона
 echo "OpenWRT ImageBuilder addon started"
-sleep infinity
