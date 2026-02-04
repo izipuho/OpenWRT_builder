704355eee73e9bc3054d46158c2bcfe1667ad4c6
izipuho <58221294+izipuho@users.noreply.github.com>
2026-02-04 11:17:25 +0300

    correct imprort

--- openwrt_builder/rootfs/app/openwrt-builder/openwrt_builder/main.py
+++ openwrt_builder/rootfs/app/openwrt-builder/openwrt_builder/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from openwrt_builder.api.v1.profiles import router
 from openwrt_builder.service.registry import Registry
 
@@ -7,5 +8,4 @@ app.state.registry = Registry()
 app.include_router(router)
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8080)
