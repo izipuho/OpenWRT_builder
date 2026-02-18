ec1456e57b4535c4f59bd916e568b026150b4e2a
izipuho <58221294+izipuho@users.noreply.github.com>
2026-02-18 09:30:03 +0300

    more paths fixing

--- openwrt_builder/rootfs/app/openwrt-builder/openwrt_builder/api/ui.py
+++ openwrt_builder/rootfs/app/openwrt-builder/openwrt_builder/api/ui.py
@@ -5,7 +5,7 @@ from fastapi.staticfiles import StaticFiles
 ui_router = APIRouter(tags=["ui"])
 
 
-ui_router.mount("/static", StaticFiles(directory="/ingress"), name="static")
+ui_router.mount("/static", StaticFiles(directory="/ingress/static"), name="static")
 
 @ui_router.get("/")
 def index():
